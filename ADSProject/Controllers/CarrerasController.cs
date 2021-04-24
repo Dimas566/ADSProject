5c67479b9c1378115fe15a1ecd2a80341aac5713
Jose <jose.carabantes@creativaconsultores.com>
2021-04-24 10:19:06 -0600

    Agregué AJAX en el componente Carreras

--- ADSProject/Controllers/CarrerasController.cs
+++ ADSProject/Controllers/CarrerasController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
 
 namespace ADSProject.Controllers
 {
@@ -47,37 +48,75 @@ namespace ADSProject.Controllers
                 // segun las validaciones que le agregamos anteriormente
                 if (ModelState.IsValid)
                 {
+                    // Esta variable nos sirve para saber si una carrera ha sida
+                    // insertada correctamente.
+                    int id = 0;
                     // Si el ID es 0; entonces se esta insertando
                     if (carrera.id == 0)
                     {
-                        servicio.insertar(carrera);
+                       id = servicio.insertar(carrera);
                     }
                     else
                     {
                         // Si el ID es distinto de cero entonces estamos modificando
-                        servicio.modificar(carrera.id, carrera);
+                       id = servicio.modificar(carrera.id, carrera);
                     }
+
+                    // Si el id es mayor a cero, entonces la operacion es correcta
+                    if ( id > 0 )
+                    {
+                        // Si la operacion fue exitosa entonces devolvemos un codigo 200(Sucess)
+                        return new JsonHttpStatusResult(carrera, HttpStatusCode.OK);
+                    }
+                    else
+                    {
+                        // Si la operación no fue exitosa entonces devolvemos un codigo 202(Accepted)
+                        return new JsonHttpStatusResult(carrera, HttpStatusCode.Accepted);
+                    }
+
+                } else
+                {
+                    // Si hubo errores en la validación, entonces devolvemos todos 
+                    // los errores del modelo con un código 400 (BadRequest)
+                    IEnumerable<ModelError> allErrors = ModelState.Values.SelectMany(temp => temp.Errors);
+                    return new JsonHttpStatusResult(allErrors, HttpStatusCode.BadRequest);
                 }
-                return RedirectToAction("Index");
+               // return RedirectToAction("Index");
             }
             catch (Exception ex)
             {
-                throw;
+                return new JsonHttpStatusResult(carrera, HttpStatusCode.InternalServerError);
+               // throw;
             }
         }
 
-        [HttpGet]
-        public ActionResult Delete(int id)
+        [HttpPost]
+        public JsonResult Delete(int id, string operacion)
         {
             try
             {
-                // Eliminar un 
-                servicio.eliminar(id);
-                return RedirectToAction("Index");
+                // variable que permite controlar si fue eliminado correctamente
+                bool correcto = false;
+                // Eliminar una carrera
+                correcto = servicio.eliminar(id);
+
+                // Si la eliminación fue correcta
+                if (correcto)
+                {
+                    // Se devuelve el id del elemento eliminado
+                    // y se retonar un código 200 (sucess)
+                    return new JsonHttpStatusResult(new { id }, HttpStatusCode.OK);
+                }else
+                {
+                    // Si no se puede eliminar, entonces se retorna un código 202(accepted)
+                    return new JsonHttpStatusResult(new { id }, HttpStatusCode.Accepted);
+                }
+               // return RedirectToAction("Index");
             }
             catch (Exception ex)
             {
-                throw;
+                return new JsonHttpStatusResult(new { id }, HttpStatusCode.InternalServerError);
+                //throw;
             }
         }
     }
